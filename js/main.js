ffc7b7a5b850d1e1e7c60fa1458ade9211ef0ee9
AbdelrahmanAyman75 <156551956+AbdelrahmanAyman75@users.noreply.github.com>
2024-08-28 23:20:28 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var img = document.querySelector(".img")
+var header = document.querySelector(".header")
+
+function phone (phone){
+    img.src =phone;
+}
+
+function bgColors(color){
+    header.style.background = color;
+
+}
